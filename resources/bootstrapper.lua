4a5515440b9c504c759c3d791912527249639a97
fujidagoat <fuji2sigma@gmail.com>
2026-07-03 19:22:00 -0400

    Update bootstrapper.lua

--- resources/bootstrapper.lua
+++ resources/bootstrapper.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 142823291 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/games/mm2.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/resources/unsupported.lua"))()
 end
