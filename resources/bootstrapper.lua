df115c50d716bd06425265499d26bd6fea38172d
fujidagoat <fuji2sigma@gmail.com>
2026-07-03 19:34:55 -0400

    Update bootstrapper.lua

--- resources/bootstrapper.lua
+++ resources/bootstrapper.lua
@@ -1,5 +1,14 @@
-if game.PlaceId == 142823291 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/games/mm2.lua"))()
-else
+local scripts = {
+    [142823291] = "games/mm2.lua",
+}
+
+local path = scripts[game.PlaceId]
+if not path then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/resources/unsupported.lua"))()
+  return 
 end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/resources/loading.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/" .. path))()
+
+--https://raw.githubusercontent.com/fujidagoat/fujihub/refs/heads/main/resources/loading.lua
